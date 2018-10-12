9b4bf0d07ea921efb0716554f9fe84cb2a7a0169
sarmishta <bsarmishta@gmail.com>
2018-10-11 17:58:42 -0700

    initial

---
+++ build/precache-manifest.d23ca285dcc680073533b4de28f6af93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "32780e22884684a69c15",
+    "url": "/static/js/main.32780e22.chunk.js"
+  },
+  {
+    "revision": "0903812db7acaa271d83",
+    "url": "/static/js/1.0903812d.chunk.js"
+  },
+  {
+    "revision": "32780e22884684a69c15",
+    "url": "/static/css/main.09be79f4.chunk.css"
+  },
+  {
+    "revision": "9f9159efa21a04bbf46df698ac0a063d",
+    "url": "/index.html"
+  }
+];
